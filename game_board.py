5e239cda4fe0b0bf2b70c5d27a5fccc3879363c1
ceonjones <ceonsvoice@gmail.com>
2025-03-06 11:57:57 -0600

    "handles game grid storage and letter placement"

---
+++ game_board.py
@@ -0,0 +1,34 @@
+class GameBoard:
+    """
+    Creates the game board with the chosen size.
+    
+    Args:
+        size = the size of the board (size x size)
+    """
+    def __init__(self, size):
+        self.size = size
+        self.grid = [[" " for _ in range(size)] for _ in range(size)]
+            
+    def place_letter(self, row, col, letter):
+        """
+        If space is available, put letter on the board
+        
+        Args:
+            row: row index
+            col: column index
+            letter: which letter to place
+        Returns:
+            True if letter placed, flase if it isn's
+        """
+        if self.grid[row][col] == " ":
+            self.grid[row][col] = letter
+            return True
+        return False
+    
+    def reset(self):
+        """
+        Reset the board to empty
+        """
+        self.grid = [[" " for _ in range(self.size)] for _ in range(self.size)]
+    
+    
