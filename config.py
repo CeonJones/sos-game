89f89ce82df04adaeeb20c3fb00f09f94cb6b59e
ceonjones <ceonsvoice@gmail.com>
2025-04-17 22:53:34 -0500

    "added config file to make sure we keep track of game constants"

---
+++ config.py
@@ -0,0 +1,22 @@
+"""
+config.py
+
+Configuration constants for our SOS game.
+Centralizes key settings such as size limits, allowed letters,
+scoring rule, and game  modes.
+"""
+
+# Board sizes
+MIN_BOARD_SIZE = 3
+MAX_BOARD_SIZE = 10
+
+# Letters that are allowed
+ALLOWED_LETTERS = {'S', 'O'}
+
+# Scoring rules
+POINTS_PER_SOS = 1
+
+# Game modes
+SIMPLE_MODE = 'Simple Game'
+GENERAL_MODE = 'General Game'
+
